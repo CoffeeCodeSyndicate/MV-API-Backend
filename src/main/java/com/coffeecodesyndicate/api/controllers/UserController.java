e308d2b1597f47b64d7191ac1a568bc8786b1f62
LaneLR <laneleerichardson@gmail.com>
2025-08-29 16:14:30 -0500

    Condensed RegisteredUserController into only UserController

--- src/main/java/com/coffeecodesyndicate/api/controllers/UserController.java
+++ src/main/java/com/coffeecodesyndicate/api/controllers/UserController.java
@@ -6,14 +6,16 @@ import com.coffeecodesyndicate.api.repositories.ApplicationRepository;
 import com.coffeecodesyndicate.api.repositories.PetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping
 public class UserController {
+
     @Autowired
     private PetRepository petRepository;
 
@@ -23,19 +25,19 @@ public class UserController {
     @GetMapping("/pets")
     public List<Pet> getAllPets() {
         return petRepository.findAll();
-    };
+    }
 
     @GetMapping("/pets/{id}")
-    public Pet getPetById(@PathVariable Integer id) {
-        return petRepository.findById(id).orElse(null);
-    };
+    public ResponseEntity<Pet> getPetById(@PathVariable Integer id) {
+        Optional<Pet> pet = petRepository.findById(id);
+        return pet.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
 
-    //registered users can create an application
-    @PreAuthorize("hasRole('isRegistered')")
     @PostMapping("/applications")
     @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("hasRole('REGISTERED_USER')")
     public Application createApplication(@RequestBody Application application) {
         return applicationRepository.save(application);
     }
-
 }
