fe6d2ead0c4f28c88613bbd16f736d700854e64d
mks257 <138339735+mks257@users.noreply.github.com>
2025-09-05 15:32:50 -0500

    update UserRepository with email lookup

--- src/main/java/com/coffeecodesyndicate/api/repositories/UserRepository.java
+++ src/main/java/com/coffeecodesyndicate/api/repositories/UserRepository.java
@@ -2,5 +2,16 @@ package com.coffeecodesyndicate.api.repositories;
 
 import com.coffeecodesyndicate.api.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends JpaRepository<User, Integer> {}
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    // find user by email (case insensitive) for login
+    Optional<User> findByEmailIgnoreCase(String email);
+
+    // optional: find by username if you also want username-based login
+    Optional<User> findByUsernameIgnoreCase(String username);
+}
