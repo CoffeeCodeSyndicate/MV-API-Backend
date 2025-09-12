5e8df6db35434337f38721eba94fd871794d7469
LaneLR <laneleerichardson@gmail.com>
2025-09-12 15:01:52 -0500

    Make it so users' email, passwords and usernames must be not null, and usernames must be unique

--- src/main/java/com/coffeecodesyndicate/api/models/User.java
+++ src/main/java/com/coffeecodesyndicate/api/models/User.java
@@ -11,8 +11,13 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(nullable = false, unique = true)
     private String username;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false)
     private String email;
 
     //isRegistered means they can adopt
